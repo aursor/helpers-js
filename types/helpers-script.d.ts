8f1c5be13e94cab3c31e8e27aee06c982a62ac74
谢展鹏 <xzp@zjwit.net>
2023-03-31 09:23:46 +0800

    更新ts描述文件

--- types/helpers-script.d.ts
+++ types/helpers-script.d.ts
@@ -1,26 +1,32 @@
 declare module "helpers-script" {
   export namespace Validates {
-    export function isChinesePhone(value: string, reg?: RegExp): boolean
-    export function isEmail(value: string, reg?: RegExp): boolean
-    export function isValidValue(value: any): boolean
-    export function isChinesePhone(collections: object): boolean
+    export function isChinesePhone(value: string, reg?: RegExp): boolean;
+    export function isEmail(value: string, reg?: RegExp): boolean;
+    export function isValidValue(value: any): boolean;
+    export function isChinesePhone(collections: object): boolean;
   }
   export namespace Parsers {
-    export function parserUrl(u: string): object
+    export function parserUrl(u: string): object;
   }
   export namespace Handlers {
     export function slice<T>(
       arr: T[],
       startIndex?: number,
       endIndex?: number
-    ): T[] | never
+    ): T[] | never;
     export function findBy<T>(
       arr: Array<T>,
       key: keyof T,
       value: any,
       multip?: boolean
-    ): Array<T> | T | undefined
+    ): Array<T> | T | undefined;
 
-    export function randomColor(): string
+    export function stringReplace(
+      value: string,
+      index: number,
+      inserted: string
+    ): string;
+
+    export function randomColor(): string;
   }
 }
